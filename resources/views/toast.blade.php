ad664557d7c49a796be475e046e3a3929ae091b1
Prajwal Hallale <22263436+prajwal89@users.noreply.github.com>
2024-12-22 17:51:22 +0530

    remove deprecated files and clean up the project structure

--- resources/views/toast.blade.php
+++
@@ -1,9 +0,0 @@
-@if (session()->has('lara-toast'))
-    <div class="toast-container"></div>
-    <script>
-        let toast = @json(session('lara-toast'));
-        document.addEventListener('DOMContentLoaded', function() {
-            showToast(toast.type, toast.title, toast.description, toast.autoCloseInMs);
-        });
-    </script>
-@endif
