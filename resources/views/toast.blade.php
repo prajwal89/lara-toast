36f434c0cbf172d44cece206cda686f9c3daabed
Prajwal Hallale <22263436+prajwal89@users.noreply.github.com>
2024-12-26 12:00:23 +0530

    clear session data for LaraToast notifications after display

--- resources/views/toast.blade.php
+++ resources/views/toast.blade.php
@@ -6,4 +6,7 @@
             showToast(toast.type, toast.title, toast.description, toast.autoCloseInMs);
         });
     </script>
+    @php
+        session()->forget('lara-toast')
+    @endphp
 @endif
