9a9d574009d4736c419c35f6af66ee2c8a15e1ce
prajwal89 <22263436+prajwal89@users.noreply.github.com>
2025-04-09 23:39:59 +0530

    use const

--- resources/views/toast.blade.php
+++ resources/views/toast.blade.php
@@ -1,12 +1,12 @@
 @if (session()->has('lara-toast'))
     <div class="toast-container"></div>
     <script>
-        let toast = @json(session('lara-toast'));
+        const toast = @json(session('lara-toast'));
         document.addEventListener('DOMContentLoaded', function() {
             showToast(toast.type, toast.title, toast.description, toast.autoCloseInMs);
         });
     </script>
     @php
-        session()->forget('lara-toast')
+        session()->forget('lara-toast');
     @endphp
 @endif
